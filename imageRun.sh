5d3efc0d8a5608660b171d3a96136911eb4a8663
William Zink <wzinc@apollo.xli.pw>
2017-04-12 16:53:07 -0400

    bug fixes; layer7 size optimization

---
+++ imageRun.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+./testImages.sh 10 &
+./testImages.sh 20 &
+./testImages.sh 30 &
+./testImages.sh 40 &
+./testImages.sh 50 &
+./testImages.sh 60 &
+./testImages.sh 70 &
+./testImages.sh 80 &
+./testImages.sh 90 &
